8d0a7350814f8d200fe00e611814754b4408fb1d
Parag <kadyanparag@gmail.com>
2024-03-01 02:01:57 +0530

    change in async handler

--- src/utils/asyncHandler.js
+++ src/utils/asyncHandler.js
@@ -1,8 +1,15 @@
 const asyncHandler = (requestHandler) => {
-    return (req,res,next) => {
-        Promise.resolve(requestHandler(req,res,next)). 
-        catch((err)=>next(err))
-    }
-}
-
+    return async (req, res, next) => {
+        try {
+            await Promise.resolve(requestHandler(req, res, next));
+        } catch (error) {
+            const statusCode = error.statusCode  ;
+            const errorMessage = error.message  ;
+            res.status(statusCode).json({
+                success: false,
+                error: errorMessage
+            });
+        }
+    };
+};
 export {asyncHandler}
